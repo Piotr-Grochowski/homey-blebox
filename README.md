0c1e37d09661fcff3015991a9f5e5baab6f8d180
Piotr Grochowski <piotr@MacBook-Pro-Piotr.local>
2019-06-12 12:24:31 +0200

    - Added dimmerBox support - Bugfix to switchBox on settings change

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Blebox.eu is a brand of high-quality products related to home automation. Create
 
 At the moment this app supports the following devices:
 - switchBox - this device allows you to wirelessly turn on or off electric devices powered by network voltage of 230V and up to 3kW of power. You can control the load via mobile devices and/or personal computers wherever you are.
+- dimmerBox - dimmerBox is the first device in the world which allows you to switch and dim 230V lights in your home or office. Control your lights from a smartphone or tablet, no matter where you are.
--- drivers/dimmerBox/device.js
+++ drivers/dimmerBox/device.js
@@ -1,15 +1,20 @@
 const Homey = require('homey');
 const util = require('/lib/util.js');
 
-module.exports = class switchBoxDevice extends Homey.Device {
+module.exports = class dimmerBoxDevice extends Homey.Device {
+
 
 	// Device init
 	onInit() {
+		// Stores last Dim Level > 0. In the beginning it's 100%
+		this.previousDimLevel = 100;
+
 		// Enable device polling
 		this.pollDevice(this.getSetting('poll_interval'));
 
-		// register a capability listener
+		// register capability listeners
 		this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+		this.registerCapabilityListener('dim', this.onCapabilityDim.bind(this));
 	}
 
 	// Cancel pooling when device is deleted
@@ -23,11 +28,18 @@ module.exports = class switchBoxDevice extends Homey.Device {
 
         if(value==true)
         {
+			var hexBrightness = Math.round(this.previousDimLevel*255).toString(16);
+			if(this.getSetting('dimOn') != 'previous' )
+			{
+				hexBrightness = Math.round(this.getSetting('dimOn')*255/100).toString(16);
+			}
+
 			// Turn on the device
-			util.sendGetCommand('/s/1',this.getSetting('address'))
+			util.sendGetCommand('/s/'+hexBrightness,this.getSetting('address'))
 			.catch(error => {
 				// Error occured
 				// Set the device as unavailable
+				this.log(error);
 				this.setUnavailable(Homey.__("device_unavailable"));
 				clearInterval(this.pollingInterval);
 				clearInterval(this.pingInterval);
@@ -41,17 +53,39 @@ module.exports = class switchBoxDevice extends Homey.Device {
 			// Turn off the device
 			util.sendGetCommand('/s/0',this.getSetting('address'))
 			.catch(error => {
+				this.log(error);
 				// Error occured
 				// Set the device as unavailable
 				this.setUnavailable(Homey.__("device_unavailable"));
 				clearInterval(this.pollingInterval);
 				clearInterval(this.pingInterval);
 
-				// check if device becomes available every 30s
+				// check if device becomes available every 60s
 				this.pingDevice();			
 			})
 		}
 	}
+
+	// this method is called when the Device has requested a state change (turned on or off)
+	async onCapabilityDim( value, opts ) {
+
+		var hexBrightness = Math.round(value*255).toString(16);
+
+		// Dim the device
+		util.sendGetCommand('/s/'+hexBrightness,this.getSetting('address'))
+		.catch(error => {
+			// Error occured
+			// Set the device as unavailable
+			this.log(error);
+			this.setUnavailable(Homey.__("device_unavailable"));
+			clearInterval(this.pollingInterval);
+			clearInterval(this.pingInterval);
+	
+			// check if device becomes available every 60s
+			this.pingDevice();			
+		})
+	}
+
 
 	async onSettings( oldSettingsObj, newSettingsObj, changedKeysArr, callback ) {
 		// run when the user has changed the device's settings in Homey.
@@ -68,18 +102,30 @@ module.exports = class switchBoxDevice extends Homey.Device {
 		// Check the physical device state and update Homey's device state
 		this.pollingInterval = setInterval(() => {
 			// Read the device state
-			util.sendGetCommand('/api/relay/state',this.getSetting('address'))
+			util.sendGetCommand('/api/dimmer/state',this.getSetting('address'))
 			.then(result => {
 				// On success - update Homey's device state
-				var state = false;
-				if(result[0].state==1) state = true;
-				
-				if (state != this.getCapabilityValue('onoff')) {
-					this.setCapabilityValue('onoff', state)
+				var brightness = Math.round(result.dimmer.desiredBrightness/255*100)/100;
+				var onState = false;
+				if(brightness>0)
+				{
+					this.previousDimLevel = brightness;
+					onState = true;
+				}
+				if (brightness != this.getCapabilityValue('dim')) {
+					this.setCapabilityValue('dim', brightness)
 						.catch( err => {
 							this.error(err);
 						})
 				}
+
+				if (onState != this.getCapabilityValue('onoff')) {
+					this.setCapabilityValue('onoff', onState)
+						.catch( err => {
+							this.error(err);
+						})
+				}
+				
 			})
 			.catch(error => {
 				this.setUnavailable(Homey.__("device_unavailable"));
@@ -96,7 +142,7 @@ module.exports = class switchBoxDevice extends Homey.Device {
 		this.pingInterval = setInterval(() => {
 			util.sendGetCommand('/api/device/state',this.getSetting('address'))
 			.then(result => {
-				if(result.device.type=='switchBox' && result.device.id==this.getData().id)
+				if(result.device.type=='dimmerBox' && result.device.id==this.getData().id)
 				{
 			  		this.setAvailable();
 			  		this.pollDevice(this.getSetting('poll_interval'));
